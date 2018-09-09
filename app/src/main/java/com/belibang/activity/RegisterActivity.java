8882c633dbd3cd880ffd4e3218d2d1f36637e4e4
Indra Pramana <indra@ide.web.id>
2018-09-09 15:34:33 +0700

    update 09092018

---
+++ app/src/main/java/com/belibang/activity/RegisterActivity.java
@@ -0,0 +1,6 @@
+package com.belibang.activity;
+
+import android.app.Activity;
+
+public class RegisterActivity extends Activity {
+}
---
+++ app/src/main/java/com/belibang/activity/SplashActivity.java
@@ -0,0 +1,6 @@
+package com.belibang.activity;
+
+import android.app.Activity;
+
+public class SplashActivity extends Activity {
+}
